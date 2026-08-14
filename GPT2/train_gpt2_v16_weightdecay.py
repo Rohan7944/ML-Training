90d9e339f1387d7e54d50472ab935777f57f1045
Jain <rohankjain@deloitte.com>
2026-08-14 19:55:50 +0530

    progress

---
+++ GPT2/train_gpt2_v16_weightdecay.py
@@ -0,0 +1,300 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import math
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1 # flag for this module to use residual init
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        # not really a bias, but more of a mask, but following openai/hf naming though
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                     .view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
+        # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs=C=768 channels in the Transformer
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        # attention (materializes the large (T,T) matrix for all the queries and keys)
+        # Flash attention ---------------------------------------------------
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+        # att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        # att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        # att = F.softmax(att, dim=-1)
+        # y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        # -------------------------------------------------------------------
+        y = y.transpose(1,2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        # output projection
+        y = self.c_proj(y)
+        return y
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc   = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu   = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1 # flag for this module to use residual init
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x)) # communication
+        x = x + self.mlp(self.ln_2(x)) # computation
+
+        return x
+
+@dataclass
+class GPTConfig:
+    block_size: int = 1024 # max sequence length
+    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    n_embd: int = 768 # embedding dimension
+
+class GPT(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = nn.LayerNorm(config.n_embd),
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        # weight sharing scheme
+        # 30% parameters being saved by sharing the embedding and the classifier weights
+        self.transformer.wte.weight = self.lm_head.weight
+
+        # init params
+        self.apply(self._init_weights) # iterate over all modules and apply _init_weights
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            # typically, std is 1/sqrt(fan_in) for linear layers but in gpt-2 they use 0.02
+            # as 1/(768^0.5) = 0.036 for 124M model so 0.02 is in the vicinity
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                # Using the scaling factor of 1/sqrt(number of layers(n)) = n**-0.5 for residual init
+                # 2 times-Every layer has 2 blocks that add to residual pathway(attention and MLP)
+                std *= (2 * self.config.n_layer) ** -0.5 # residual init
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std) 
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias) # by default bias is initialized with uniform
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, targets=None):
+        # idx is of shape (B, T)
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+        # forward the token and posisition embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device) # shape (T)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (T, n_embd)
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (B, T, n_embd)
+        x = tok_emb + pos_emb
+        # forward the blocks of the transformer
+        for block in self.transformer.h:
+            x = block(x)
+        # forward the final layernorm and the classifier
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x) # (B, T, vocab_size)
+        loss = None
+        if targets is not None:
+            # compute the loss
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+        return logits, loss
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model weights from huggingface"""
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+        }[model_type]
+        config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
+        config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
+        # create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
+# -----------------------------------------------------------------------------------------------
+# get a single data batch
+import tiktoken
+
+class DataLoaderLite:
+
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+        # at init load tokens from disk and store them in memory
+        with open('input.txt', 'r') as f:
+            text = f.read() # read the entire file into a string
+        enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens) # tokenizing the file
+        print(f"loaded {len(self.tokens)} tokens from input.txt")
+        print(f"1 epoch = {len(self.tokens) // (B*T)} batches") # unique batches per epoch
+        #state
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position + B*T + 1] # +1 because we need ground truth for the last token
+        x = (buf[:-1].view(B, T)) # (B, T) inputs
+        y = (buf[1:].view(B, T)) # (B, T) targets
+        # advance the position in the tensor
+        self.current_position += B*T
+        # if loading the next batch is out of bounds, reset
+        if self.current_position + (B*T + 1) >= len(self.tokens):
+            self.current_position = 0
+        return x, y
+
+# --------------------------------------------------------------------------------------------------
+# attempt to autodetect device
+
+import time
+
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda:0" # Added :0 for 1660 ti
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = "mps"
+print(f"using device: {device}")
+
+# Config as per 1660ti, use 2^x for values of B otherwise it runs inefficiently
+train_loader = DataLoaderLite(B=1, T=1024) # 1 example per batch, sequence length 1024
+
+# enable TF32, works only with TF32 supported GPU architectures
+torch.set_float32_matmul_precision('high')
+
+# ---------------------------------------------------------------------------------------------------
+# For reproducibility
+torch.manual_seed(1337) # set the random seed for reproducibility
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(1337) # set the random seed for all GPUs
+
+# --------------------------------------------------------------------------------------------------------
+# get the logits
+# model = GPT.from_pretrained('gpt2') # initialize from gpt2 model weights from huggingface
+model = GPT(GPTConfig(vocab_size=50304)) # initialize a new model with the same config as GPT-2 (random model)
+model.to(device)
+model = torch.compile(model) # compile the model for faster training
+
+# ---------------------------------------------------------------------------------------------------
+# Learning rate scheduler - Cosine decay learning schedule with warmup
+
+max_lr = 6e-4 # maximum learning rate
+min_lr = max_lr * 0.1 # minimum learning rate (10% of max_lr)
+warmup_steps = 10
+max_steps = 50
+def get_lr(it):
+    # 1. Linear warmup for warmup_iter steps
+    if it < warmup_steps:
+        return max_lr * (it + 1) / warmup_steps
+    # 2. If it > lr_decay_iters, return min learning rate
+    if it > max_steps:
+        return min_lr
+    # 3. In between, use cosine decay down to min learning rate
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and ends at 0
+    return min_lr + coeff * (max_lr - min_lr) # scale the learning rate
+
+# ---------------------------------------------------------------------------------------------------
+# optimization
+# alternative to SGD
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
+for step in range(max_steps):
+    t0 = time.time()
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
+    optimizer.zero_grad() # start with 0 gradients
+    with torch.autocast(device_type=device, dtype=torch.bfloat16): # enable bf16 for faster training
+        logits, loss = model(x, y)
+    loss.backward() # adds to gradient for each parameter based on the loss
+    # clip the gradient to prevent exploding gradients
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    # determine and set the learning rate for this iteration
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+    optimizer.step() # update the parameters and decrease the loss
+    torch.cuda.synchronize() # wait for the GPU to finish before measuring time
+    t1 = time.time()
+    dt = (t1 - t0)
+    tokens_processed = train_loader.B * train_loader.T
+    tokens_per_sec = tokens_processed / dt 
+    dt *= 1000 # Convert to milliseconds
+    print(f"step {step}: loss {loss.item():.4f}, norm {norm:.4f}, time {dt:.2f} ms, tokens/sec {tokens_per_sec:.2f}")
